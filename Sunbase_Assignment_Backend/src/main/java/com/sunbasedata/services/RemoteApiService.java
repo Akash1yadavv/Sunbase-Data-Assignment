054cc5f77e5c06ff93cc1a5c54441c197ffa3e1d
Akash Yadav <107462445+Akash1yadavv@users.noreply.github.com>
2024-08-29 04:35:54 +0530

    Delete Sunbase_Assignment_Backend directory

--- Sunbase_Assignment_Backend/src/main/java/com/sunbasedata/services/RemoteApiService.java
+++
@@ -1,9 +0,0 @@
-package com.sunbasedata.services;
-
-import com.sunbasedata.exceptions.RemoteApiException;
-
-public interface RemoteApiService {
-
-	String syncCustomersFromRemoteApi()throws RemoteApiException;
-
-}
